772a0fb1d0b191e95d60a4fc12cd12aede829234
Tanmoy Debnath <53893576+tandebnath@users.noreply.github.com>
2024-10-29 01:40:03 -0500

    Set up GitHub Pages deployment

--- peritext-website/next.config.mjs
+++ peritext-website/next.config.mjs
@@ -1,13 +1,11 @@
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Enables static HTML export
-    images: {
-      unoptimized: true, // Disables Next.js image optimization (necessary for GitHub Pages)
-    },
-    basePath: '/peritext', // Sets the base path to match the GitHub Pages URL path
-  };
-  
-  export default nextConfig;
-  
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/peritext', // This should match the repository name
+  assetPrefix: '/peritext/', // Ensures static assets are served correctly
+};
+
+export default nextConfig;
